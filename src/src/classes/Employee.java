31ff8caae310b74a1e9957acb0fd3730de9527a9
Konstantina155 <105597925+Konstantina155@users.noreply.github.com>
2023-01-04 00:16:59 +0200

    Update Employee.java

--- src/src/classes/Employee.java
+++ src/src/classes/Employee.java
@@ -1,17 +1,18 @@
 package classes;
 
+import java.sql.SQLException;
 import java.util.Date;
 
 public abstract class Employee {
 
-    enum employee {PE, PM, CE, CM};
-    private String firstName;
-    private String lastName;
+    public enum employee {PE, PM, CE, CM}
+    private final String firstName;
+    private final String lastName;
 
     public abstract String groupEmployer();
 
     public abstract String JobDepartment();
-    private Date beginHiringDate;
+    private final Date beginHiringDate;
 
     private String address;
 
@@ -22,6 +23,8 @@ public abstract class Employee {
     private BankInfo bankInfo;
 
     private Bonus bonus;
+
+    private EmployeesSalary salary;
 
     Employee(String firstName, String lastName, String address, int phoneNumber, Date beginHiringDate) {
         this.firstName = firstName;
@@ -59,7 +62,7 @@ public abstract class Employee {
         return this.familyState;
     }
 
-    public void setBank_Info(BankInfo info) {
+    public void setBankInfo(BankInfo info) {
         this.bankInfo = info;
     }
 
@@ -75,6 +78,14 @@ public abstract class Employee {
         return this.bonus;
     }
 
+    public void setEmployeesSalary(EmployeesSalary salary) throws SQLException{
+        this.salary = salary;
+    }
+
+    public EmployeesSalary getEmployeesSalary() {
+        return this.salary;
+    }
+
     public abstract String toString();
 
     public employee findEmployee(){
@@ -87,4 +98,39 @@ public abstract class Employee {
         }
     }
 
+    public double calculateFamilyBonus() {
+        int underageKids=0;
+
+        if (familyState == null) return 0;
+
+        if (familyState.getState().equals("married")){
+            for (int i : familyState.getKidsAges()){
+                if (i <18) underageKids +=1;
+            }
+
+            return 0.05*(underageKids+1);
+        }
+        return 0;
+    }
+
+    public void setSalary() throws SQLException {
+        String date= beginHiringDate.toString();
+        String dateParts[] = date.split("-");
+        double bonus1 = 0, basicSalary = salary.getBasicSalary(), contractSalary = salary.getContractSalary();
+
+
+        if (groupEmployer() == "Permanent"){
+            bonus1 += bonus.getFamilyBonus() + 0.15 * (2022 - Integer.parseInt(dateParts[0]));
+            if (JobDepartment() == "Educator") bonus1 += bonus.getSearchBonus();
+            basicSalary += basicSalary * bonus1;
+            contractSalary = 0;
+        } else {
+            bonus1 += getBonus().getFamilyBonus();
+            if (JobDepartment() == "Educator") bonus1 += bonus.getLibraryBonus();
+            contractSalary = contractSalary * ((2022-Integer.parseInt(dateParts[0]))*12 + 12-Integer.parseInt(dateParts[1])+1)/salary.getMonthsContract();
+            basicSalary = 0;
+        }
+
+        this.salary = new EmployeesSalary(basicSalary,contractSalary,salary.getMonthsContract(),salary.getSalaryID());
+    }
 }
