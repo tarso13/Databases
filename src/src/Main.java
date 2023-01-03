a592070fb43a5aeee7fbe3e76a4ef8dfbd06059b
Konstantina155 <105597925+Konstantina155@users.noreply.github.com>
2023-01-04 00:14:24 +0200

    Update Main.java

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,9 +1,7 @@
-import classes.ContractorEducator;
-import classes.ContractorManager;
-import classes.PermanentEducator;
-import classes.PermanentManager;
+import classes.*;
 import server.ServerRequest;
 
+import java.sql.Date;
 import java.sql.SQLException;
 
 public class Main {
@@ -12,18 +10,20 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         request = new ServerRequest();
 
-        PermanentEducator permanentEducator = request.getPE(5);
-        System.out.println(permanentEducator.toString());
+        EmployeesSalary salary = new EmployeesSalary(1500,600,0,0);
+        salary.setInitialSalary(1500,600);
 
-        request.hirePM(10,12);
+        Bonus bonus = new Bonus(0,0.5,0.10,0);
+        //request.login("username","password");
+        //request.changeAddress("aaaaa",1);
+        //request.changePhoneNumber(21022222,15);
+        //request.changeFamilyState("married",3,"15 15 18", 5,3,salary);
+        PermanentEducator permanentEducator= new PermanentEducator("firstName", "lastName", "address", 1234, new Date(2022-1-1),25);
+        permanentEducator.setEmployeesSalary(salary);
+        permanentEducator.setBonus(bonus);
+        BankInfo bankInfo = request.insertBankInfo(873833838,"alpha");
+        FamilyState state = request.insertFamilyState("unmarried",0,"",salary);
+        request.hirePermanentEmployee(permanentEducator,"aaaa","bbb",bankInfo,state);
 
-//        PermanentManager permanentManager = request.getPM(1);
-//        System.out.println(permanentManager.toString());
-
-//        ContractorEducator contractorEducator = request.getCE(7);
-//        System.out.println(contractorEducator.toString());
-
-//        ContractorManager contractorManager = request.getCM(6);
-//        System.out.println(contractorManager.toString());
     }
 }
