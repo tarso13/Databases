6caf2797a87d3c31839c7eb02c26744d2cbd3284
KONSTANTINA PAPAFRAGKAKI <csd4331@csd.uoc.gr>
2023-01-16 13:23:54 +0200

    Gui.loginPage in main

--- src/src/Main.java
+++ src/src/Main.java
@@ -2,28 +2,21 @@ import classes.ContractorEducator;
 import classes.ContractorManager;
 import classes.PermanentEducator;
 import classes.PermanentManager;
+import classes.Employee;
+
+import gui.GUI;
 import server.ServerRequest;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.sql.SQLException;
 
 public class Main {
     static ServerRequest request;
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         request = new ServerRequest();
 
-        PermanentEducator permanentEducator = request.getPE(5);
-        System.out.println(permanentEducator.toString());
-
-        request.hirePM(10,12);
-
-//        PermanentManager permanentManager = request.getPM(1);
-//        System.out.println(permanentManager.toString());
-
-//        ContractorEducator contractorEducator = request.getCE(7);
-//        System.out.println(contractorEducator.toString());
-
-//        ContractorManager contractorManager = request.getCM(6);
-//        System.out.println(contractorManager.toString());
+        GUI.loginPage();
     }
 }
